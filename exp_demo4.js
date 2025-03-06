5d4c89c60a8e99cc62f8d64ecbce299397aa2a61
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-05 16:25:23 -0800

    Add GET endpoints for '/home' and '/about' in exp_demo4.js

--- exp_demo4.js
+++ exp_demo4.js
@@ -1,8 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+app.get("/home", (req, res) => {
+  res.send("Home Page");
+});
 
+app.get("/about", (req, res) => {
+  res.send("About");
+});
 
 app.listen(3000);
-
-
