58ab3ec7289b497fab836887afaa8204d6dc45d2
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-05 16:22:19 -0800

    Add request logger middleware and standardize quotation style in routes

--- exp_demo7.js
+++ exp_demo7.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // Simple request time logger
+app.use((req, res, next) => {
+  console.log("A new request received at " + Date.now());
 
- 
-    // This function call tells that more processing is
-    // required for the current request and is in the next middleware
-    //function/route handler.
-    
-
-app.get('/home', (req, res) => {
-    res.send('Home Page');
+  // This function call tells that more processing is
+  // required for the current request and is in the next middleware
+  //function/route handler.
+  next();
 });
-app.get('/about', (req, res) => {
-    res.send('About');
+
+app.get("/home", (req, res) => {
+  res.send("Home Page");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
 app.listen(3000);
-
-
